15ca40130b1616c66e101dbadcfe3a4eb8752ab5
SpencerTower <spencerjtower1@gmail.com>
2022-08-16 15:48:48 -0700

    adds Card component with styling to display meal items

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,3 +1,7 @@
-const Card = (props) => {};
+import classes from './Card.module.css';
+
+const Card = (props) => {
+    return <div className={classes.card}>{props.children}</div>;
+};
 
 export default Card;
