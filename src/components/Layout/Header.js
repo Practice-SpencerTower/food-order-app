a362f187cc0d0724c9c8361420d8e23a499ceb77
SpencerTower <spencerjtower1@gmail.com>
2022-08-16 14:07:31 -0700

    adds Header component to app

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
 const Header = props => {
     return (
         <>
-            <header>
+            <header className={classes.Header}>
                 <h1>React Meals</h1>
                 <button>Cart</button>
             </header>
-            <div>
+            <div className={classes['main-image']}>
                 <img src={mealsImage} alt="A table full of delicious food!"/>
             </div>
         </>
