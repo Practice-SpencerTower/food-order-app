0384d999e1adfba92fc9ad6e1398e7bc35bf2b54
SpencerTower <spencerjtower1@gmail.com>
2022-08-16 14:47:41 -0700

    applys styling to header cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,12 +3,12 @@ import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
     return (
-        <button>
-            <span>
+        <button className={classes.button}>
+            <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span>
+            <span className={classes.badge}>
                 3
             </span>
         </button>
