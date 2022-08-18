8bbcf1c8e6342b88aa9db2c89e191c65277afa9a
SpencerTower <spencerjtower1@gmail.com>
2022-08-18 16:20:42 -0700

    adds cartReducer to add / remove items from cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,15 +1,38 @@
+import { useReducer } from 'react';
+
 import CartContext from './cart-context';
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount =
+            state.totalAmount + action.item.price * action.item.amount;
+    }
+    return defaultCartState;
+};
+
 const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(
+        cartReducer,
+        defaultCartState
+    );
+
     const addItemHandler = (item) => {
-        console.log('ADDITEMHANDLER TRIGGERED');
+        dispatchCartAction({ type: 'ADD', item: item });
     };
 
-    const removeItemHandler = (id) => {};
+    const removeItemHandler = (id) => {
+        dispatchCartAction({ type: 'REMOVE', id: id });
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemHandler,
         removeItem: removeItemHandler,
     };
