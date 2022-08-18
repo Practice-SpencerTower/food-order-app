5df51b61cbfe7a15dffb889eb01eac7e8c1ac5fc
SpencerTower <spencerjtower1@gmail.com>
2022-08-18 15:38:36 -0700

    removes context from Cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,5 @@
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
-import { useContext } from 'react';
-import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
     const cartItems = (
@@ -15,28 +13,22 @@ const Cart = (props) => {
     );
 
     return (
-        <CartContext.Consumer>
-            {({ totalAmount, addItem }) => (
-                <Modal>
-                    {cartItems}
-                    <div className={classes.total}>
-                        <span>Total Amount</span>
-                        <span>35.62</span>
-                    </div>
-                    <div className={classes.actions}>
-                        <button
-                            className={classes['button-alt']}
-                            onClick={props.hideCartHandler}
-                        >
-                            Close
-                        </button>
-                        <button className={classes.button} onClick={addItem}>
-                            Order
-                        </button>
-                    </div>
-                </Modal>
-            )}
-        </CartContext.Consumer>
+        <Modal>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className={classes.actions}>
+                <button
+                    className={classes['button-alt']}
+                    onClick={props.hideCartHandler}
+                >
+                    Close
+                </button>
+                <button className={classes.button}>Order</button>
+            </div>
+        </Modal>
     );
 };
 
