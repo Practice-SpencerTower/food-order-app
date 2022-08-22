4317313d0cc1c427d4610b070a2193d08b334974
SpencerTower <spencerjtower1@gmail.com>
2022-08-22 12:29:33 -0700

    updates cart reducer to update cart items so each cart item is displayed once with the item count

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,9 +9,30 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount =
             state.totalAmount + action.item.price * action.item.amount;
+
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        ); // finds index of item in an array
+        const existingCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount,
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem; // override existing item with updated item
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
     }
     return defaultCartState;
 };
