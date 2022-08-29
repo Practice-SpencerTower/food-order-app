973d24bc072e18bf71c8c2b8fec11df10777be6d
SpencerTower <spencerjtower1@gmail.com>
2022-08-29 12:36:46 -0700

    adds state and check for loading, renders message if page loading, adds loading styling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,6 +6,7 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchMeals = async () => {
@@ -26,9 +27,19 @@ const AvailableMeals = () => {
                 });
             }
             setMeals(loadedMeals);
+            setIsLoading(false);
         };
         fetchMeals();
     }, []);
+
+    if (isLoading) {
+        return (
+            <section className={classes.mealsLoading}>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+
     const mealsList = meals.map((meal) => (
         <MealItem
             id={meal.id}
