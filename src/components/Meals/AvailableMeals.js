369f9100d1e8278406b2daa0c5bca97296aa4a98
SpencerTower <spencerjtower1@gmail.com>
2022-08-29 12:07:24 -0700

    adds fetchMeals to query firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
+
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
+    useEffect(() => {
+        const fetchMeals = async () => {
+            fetch(
+                'https://react-http-9f7be-default-rtdb.firebaseio.com/meals.json'
+            ).then(() => {});
+        };
+        fetchMeals();
+    }, []);
     const mealsList = DUMMY_MEALS.map((meal) => (
         <MealItem
             id={meal.id}
