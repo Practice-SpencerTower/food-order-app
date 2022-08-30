ccf13aaf96c89d083acec90df5b17d7a58a2bae5
SpencerTower <spencerjtower1@gmail.com>
2022-08-30 16:04:24 -0700

    adds error messages for invalid inputs

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -53,18 +53,22 @@ const Checkout = (props) => {
             <div className={classes.control}>
                 <label htmlFor="name">Your Name</label>
                 <input type="text" id="name" ref={nameInputRef} />
+                {!formInputValidity && <p>Please enter a valid name!</p>}
             </div>
             <div className={classes.control}>
                 <label htmlFor="street">Street</label>
                 <input type="text" id="street" ref={streetInputRef} />
+                {!formInputValidity && <p>Please enter a valid street!</p>}
             </div>
             <div className={classes.control}>
                 <label htmlFor="postal">Postal Code</label>
                 <input type="text" id="postal" ref={postalInputRef} />
+                {!formInputValidity && <p>Please enter a valid postal code!</p>}
             </div>
             <div className={classes.control}>
                 <label htmlFor="city">City</label>
                 <input type="text" id="city" ref={cityInputRef} />
+                {!formInputValidity && <p>Please enter a valid city!</p>}
             </div>
             <div className={classes.actions}>
                 <button type="button" onClick={props.hideCartHandler}>
